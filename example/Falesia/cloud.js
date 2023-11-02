84c2f29e63b09297850fa70ee935c1615c431eb1
Jordan Elias <149501960+EncontrosAC@users.noreply.github.com>
2023-11-02 18:12:08 -0300

    final commit

---
+++ example/Falesia/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=24 +south +ellps=WGS84 +datum=WGS84 +units=m",
+    "points": 14695066,
+    "boundingBox": {
+        "lx": 666216.371263,
+        "ly": 9487641.632956,
+        "lz": -5.604692,
+        "ux": 668100.491329,
+        "uy": 9489525.753022,
+        "uz": 1878.515373
+    },
+    "tightBoundingBox": {
+        "lx": 666324.599391,
+        "ly": 9487720.071396,
+        "lz": -0.404692,
+        "ux": 667406.736304,
+        "uy": 9488504.367981,
+        "uz": 51.434372
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 14.719688,
+    "scale": 0.010000,
+    "hierarchyStepSize": 5
+}
